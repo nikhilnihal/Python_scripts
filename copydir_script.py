8bbf62f44cd16b0124aeffc9179c982cec0ffedc
NIKHIL NIHAL <nikhilnihalnn2@gmail.com>
2016-09-19 12:20:03 +0530

    Add files via upload

---
+++ copydir_script.py
@@ -0,0 +1,4 @@
+import shutil 
+
+shutil.copytree('nikhildirectory', 'copyofnikhildir')
+
---
+++ fb_login_script.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("https://www.facebook.com/")
+#assert "GitHub" in driver.title
+elem = driver.find_element_by_name("email")
+elem.clear()
+elem.send_keys("nikhil@gmail.com")
+elem.send_keys()
+elem1 = driver.find_element_by_name("pass")
+elem1.clear()
+elem1.send_keys("")
+elem1.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
