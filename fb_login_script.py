b3535432d167e024a1e2e7d1c8b5390789f19823
NIKHIL NIHAL <nikhilnihalnn2@gmail.com>
2016-09-19 12:23:13 +0530

    Update fb_login_script.py
    Anyone can run this script just by editing the email and password .
    By running one can login his /her facebook account

--- fb_login_script.py
+++ fb_login_script.py
@@ -10,6 +10,6 @@ elem.send_keys("nikhil@gmail.com")
 elem.send_keys()
 elem1 = driver.find_element_by_name("pass")
 elem1.clear()
-elem1.send_keys("")
+elem1.send_keys(" ur password ")
 elem1.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
